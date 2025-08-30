124167b477d382cb30fb5623d7b0bc623715924a
74717sarang <patilsarang13520@gmail.com>
2025-08-30 16:55:42 +0530

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class Class1
+    {
+
+        public Class1()
+        {
+               Console.WriteLine("Hello, Class1!");
+        }
+
+        
+    }
+}
